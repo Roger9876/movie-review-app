29747fad50864a7b0de7e55d0f2ca7b46e80cfa8
Raviraj Khopade <rkhopade@hawk.iit.edu>
2024-06-14 13:37:08 -0500

    added CORS configuration

--- backend/movies/src/main/java/developer/raviraj/movies/MoviesApplication.java
+++ backend/movies/src/main/java/developer/raviraj/movies/MoviesApplication.java
@@ -2,14 +2,29 @@ package developer.raviraj.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@SuppressWarnings("null")
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedOrigins("*")
+						.allowedOrigins("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
